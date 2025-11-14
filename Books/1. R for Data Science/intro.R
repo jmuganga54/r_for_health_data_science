d703d366d5c9160c07db4806dd08adec5f6be79e
Joseph Muganga <jmuganga544@gmail.com>
2025-11-14 10:35:35 +0000

    Update README file

---
+++ Books/1. R for Data Science/intro.R
@@ -0,0 +1,15 @@
+install.packages("tidyverse")  
+
+library("tidyverse") 
+
+tidyverse_update() # updating tidyverse package 
+
+install.packages(
+  c("arrow", "babynames", "curl", "duckdb", "gapminder", 
+    "ggrepel", "ggridges", "ggthemes", "hexbin", "janitor", "Lahman", 
+    "leaflet", "maps", "nycflights13", "openxlsx", "palmerpenguins", 
+    "repurrrsive", "tidymodels", "writexl")
+)
+
+# Running R code
+1 + 2
